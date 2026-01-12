c69c8ec71cf4b6ee2b73c5622638d3e50a1e1432
jmfernandezg99 <jmfernandezg99@gmail.com>
2026-01-12 11:31:56 +0100

    integracionFuncional

---
+++ frontend-app/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router' // <--- Importamos el router
+
+const app = createApp(App)
+
+app.use(router) // <--- Le decimos a Vue que lo use
+app.mount('#app')
