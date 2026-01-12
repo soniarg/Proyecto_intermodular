617f13391e896124d83524a98b4dfc2dc7b2c9aa
Pablo-006 <pabsengom@alu.edu.gva.es>
2026-01-12 10:08:38 +0100

    Inicio sprint3 - crear una nueva migracion (tabla ubicaciones) y su respectivo modelo

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function locations()
+    {
+        return $this->hasMany(CustomerLocation::class);
+    }
 }
