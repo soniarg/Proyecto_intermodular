6a8ef306ef2990f4bc55fac0d61e5f80e5f33bf2
Pablo-006 <pabsengom@alu.edu.gva.es>
2026-01-19 09:09:21 +0100

    Comentar archivo configuracion mapas (falta por terminar de comentar)

--- backend/resources/views/vendor/maps/components/leaflet.blade.php
+++ backend/resources/views/vendor/maps/components/leaflet.blade.php
@@ -1,9 +1,24 @@
+<!-- Este enlace importa todas las configuraciones visuales para el mapa, sin esto, el mapa sería un desastre -->
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
       integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
       crossorigin=""/>
 
+<!--Aquí se añaden los estilos css al mapa que se hayan definido en el html. En caso
+de no haberse definido ningún estilo en el html, se define por defecto la altura del mapa
+que es la altura de la pantalla -->
 <style>
+    /* Aquí es donde se selecciona el id al que se le van a asignar los estilos
+    Se usa la almohadilla '#' para hacer referencia a que se selecciona un id,
+    y entre doble llave, se escoge el id que tendrá el mapa ($mapId). Esta variable
+    genera ids aleatorios, para poder tener varios mapas en una misma página 
+    Las dobles llaves sirven para imprimir valores de las variables. Sin ellas
+    Blade buscaría un id que literalmente se llamara $mapId*/
     #{{$mapId}} {
+        /* La lógica de programacion como ifs o bucles etc, empieza con @ */
+        /* La función isset sirve para comprobar que una variable exista
+        y que tenga un valor. Aquí, en caso de que la variable $attributes tenga un estilo,
+        se usan los estilos para el mapa, en caso contrario, se define una altura
+        por defecto. La variable attributes se crea al hacer uso del componente del mapa*/
     @if(! isset($attributes['style']))
         height: 100vh;
     @else
@@ -12,13 +27,23 @@
     }
 </style>
 
+/* Aquí se define el div donde se colocará el mapa. Aunque realmente donde se definen los divs 
+es en el html, este div es necesario para poder colocar el mapa y aplicar toda la lógica de programacion 
+y estilos. Sin este div, no se podría visualizar el mapa */
+
+/* El id del mapa en caso de no ser asignado, se le asigna automáticamente uno gracias a Laravel
+y la clase del componente */
 <div id="{{$mapId}}" @if(isset($attributes['class']))
  class='{{ $attributes["class"] }}'
 @endif
 ></div>
 
+/* Este script carga el JavaScript de Leaflet para acceder a funciones exclusivas para editar el mapa */
 <script src="{{'https://unpkg.com/leaflet@' . $leafletVersion . '/dist/leaflet.js'}}"
-        crossorigin=""></script>
+        crossorigin="">
+</script>
+
+/* En este script se define toda la lógica de programación acerca del mapa */
 <script>
     var mymap = L.map('{{$mapId}}', {
         minZoom: 7,            
