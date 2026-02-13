a1f18d39c0b2e1b1fd1a6be39696ae5a4a9ebc6f
soniarg <soniaroigmarti@gmail.com>
2026-02-13 10:29:52 +0100

    para desplegar

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,3 +8,8 @@ use App\Http\Controllers\MapController;
 // });
 
 Route::get('/', [MapController::class, 'index'])->name('home');
+
+// Esto hace que cualquier ruta que no sea de la API devuelva el Vue
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
