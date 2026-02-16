7b2f49ff7ea9d78f2321731165f8a4eb5dc4558b
soniarg <soniaroigmarti@gmail.com>
2026-02-16 09:54:55 +0100

    Quitar mi intento de despliegue

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,17 +7,5 @@ use App\Http\Controllers\MapController;
 //     return view('welcome');
 // });
 
-// Route::get('/', [MapController::class, 'index'])->name('home');
+Route::get('/', [MapController::class, 'index'])->name('home');
 
-// Esto hace que cualquier ruta que no sea de la API devuelva el Vue
-Route::get('/{any}', function () {
-    // Como está en public/dist/index.html, tenemos que indicarlo:
-    $path = public_path('dist/index.html'); 
-    
-    if (file_exists($path)) {
-        return file_get_contents($path);
-    }
-    
-    // Si no lo encuentra, que nos diga dónde está buscando para arreglarlo
-    return "No encuentro el index.html en: " . $path;
-})->where('any', '.*');
