2c2ff9a309eda53bf3f06894acbd54e7f6098b89
Pablo-006 <pabsengom@alu.edu.gva.es>
2026-02-22 11:50:29 +0100

    Quitar SSL de la base de datos (en azure) y quitar opciones en el database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -58,10 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => '/home/site/wwwroot/backend/DigiCertGlobalRootCA.crt.pem',
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            'options' => [],
         ],
 
         'mariadb' => [
